bc064c9d62b43224f438d853b2f03c96c3a76874
Slei03 <lei.sallyc@gmail.com>
2023-01-10 20:07:34 -0800

    change layout

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-document.addEventListener("scroll", ()=>{
+document.addEventListener("DOMContentLoaded", () => { 
+    document.getElementById("open-sal-body").style.display = "block";
+    document.getElementById("about-me-body").style.display = "none";
+    document.getElementById("contact-me-body").style.display = "none";
+})
+
+function selectSection(section){
+    sections = document.getElementsByClassName("content");
+    for(i = 0; i < sections.length; ++i){
+        document.getElementById(sections[i].id).style.display = "none";
+    }
+
+    if(section === "about-me"){
+        document.getElementById("about-me-body").style.display = "block"
+        document.getElementById("about-nav").style.textDecoration = "underline"
+        document.getElementById("contact-nav").style.textDecoration = "none"
+    }
+
+    if(section === "contact-me"){
+        document.getElementById("contact-me-body").style.display = "block"
+        document.getElementById("about-nav").style.textDecoration = "none"
+        document.getElementById("contact-nav").style.textDecoration = "underline"
+    }
+}
+
+/* document.addEventListener("scroll", ()=>{
     var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
     const greetingTop = document.getElementById("open-sal-body").offsetTop;
     const aboutTop = document.getElementById("about-me-body").offsetTop;
@@ -22,3 +47,4 @@ document.addEventListener("scroll", ()=>{
         document.getElementById("about-arrow").style.display = "block"
     }
 });
+ */
